66b50fb676f5a08f50484f73db16e6913be5ef62
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-10-08 10:55:43 +0600

    create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CONTAINER_NAME="abstechnologies_app"
+DOCKER_IMAGE_TAG="abstechnologies:$BUILD_ID"
+PORT=9191
+
+# Check if the container is running
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Stopping and removing existing container..."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+fi
+
+# Build the Docker image
+echo "Building Docker image with tag $DOCKER_IMAGE_TAG..."
+docker build -t $DOCKER_IMAGE_TAG .
+
+# Run the container
+echo "Running Docker container on port $PORT..."
+docker run -d -p $PORT:80 --name $CONTAINER_NAME $DOCKER_IMAGE_TAG
