0b412b69a5cbf7739e0c6f301294c380e698f4f2
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-10-14 14:20:13 +0600

    update

--- ansible-kubernetes-deploy.sh
+++ ansible-kubernetes-deploy.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Deploy to Kubernetes
+# Deploy to Ansible
 echo "Deploying to Ansible..."
 ansible-playbook -i inventory.ini playbook.yml --become || { echo "Failed to apply Ansible deployment! Exiting."; exit 1; }
 
 # Deploy to Kubernetes
 echo "Deploying to Kubernetes..."
-kubectl apply -f k8s-deployment.yml --validate=false || { echo "Failed to apply Kubernetes deployment! Exiting."; exit 1; }
-kubectl apply -f k8s-service.yml --validate=false || { echo "Failed to apply Kubernetes service! Exiting."; exit 1; }
-
-# Deploy to Kubernetes
-echo "Deploying to Kubernetes..."
-kubectl apply -f k8s-deployment.yml --validate=false || { echo "Failed to apply Kubernetes deployment! Exiting."; exit 1; }
-kubectl apply -f k8s-service.yml --validate=false || { echo "Failed to apply Kubernetes service! Exiting."; exit 1; }
+kubectl apply -f k8s_deployments/deployment.yml --validate=false || { echo "Failed to apply Kubernetes deployment! Exiting."; exit 1; }
+kubectl apply -f k8s_deployments/service.yaml --validate=false || { echo "Failed to apply Kubernetes service! Exiting."; exit 1; }
 
 # Log some debugging information
 echo "Currently running Docker containers:"
